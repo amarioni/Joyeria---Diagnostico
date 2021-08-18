c461eb80ddc512db4712a41d3234ae5438757b58
amarioni <83253601+amarioni@users.noreply.github.com>
2021-08-18 19:05:04 -0300

    change prroduct and productex

---
+++ scripts/miprogramacion.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#prod1").click(function(){
+        $("#exclusive").load("prod1.html");
+    });
+    $("#prod2").click(function(){
+        $("#exclusive").load("prod2.html");
+    });
+    $("#prod3").click(function(){
+        $("#exclusive").load("prod3.html");
+    });
+});
